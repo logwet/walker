72b55f5e678e408b0c1e337bfecc8631fdf9331b
logwet <logwet@users.noreply.github.com>
2022-01-18 12:55:29 +1100

    Force walking forwards

---
+++ src/main/java/me/logwet/walker/mixin/client/KeyboardInputMixin.java
@@ -0,0 +1,35 @@
+package me.logwet.walker.mixin.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.player.KeyboardInput;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Environment(EnvType.CLIENT)
+@Mixin(KeyboardInput.class)
+public abstract class KeyboardInputMixin {
+    @Redirect(
+            method = "tick",
+            at =
+                    @At(
+                            value = "FIELD",
+                            target = "Lnet/minecraft/client/player/KeyboardInput;up:Z",
+                            opcode = Opcodes.PUTFIELD))
+    private void overrideForwardInput(KeyboardInput keyboardInput, boolean value) {
+        keyboardInput.up = true;
+    }
+
+    @Redirect(
+            method = "tick",
+            at =
+                    @At(
+                            value = "FIELD",
+                            target = "Lnet/minecraft/client/player/KeyboardInput;down:Z",
+                            opcode = Opcodes.PUTFIELD))
+    private void overrideBackwardsInput(KeyboardInput keyboardInput, boolean value) {
+        keyboardInput.down = false;
+    }
+}
